f72347b36bf8c160ea572e794053d75d450095a0
Deven Bawale <dbawale@pdx.edu>
2016-08-02 23:33:39 -0700

    Basic, minimal UI concept

--- apptbook-gwt/src/main/java/edu/pdx/cs410J/dbawale/client/AppointmentBookGwt.java
+++ apptbook-gwt/src/main/java/edu/pdx/cs410J/dbawale/client/AppointmentBookGwt.java
@@ -3,12 +3,12 @@ package edu.pdx.cs410J.dbawale.client;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.*;
 import edu.pdx.cs410J.AbstractAppointment;
 
 import java.util.Collection;
@@ -18,7 +18,9 @@ import java.util.Collection;
  */
 public class AppointmentBookGwt implements EntryPoint {
   private final Alerter alerter;
-
+  HorizontalPanel hpanelmain = new HorizontalPanel();
+  VerticalPanel vpaneltab1 = new VerticalPanel();
+  TabLayoutPanel tabpanel = new TabLayoutPanel(2.0, Style.Unit.EM);
   @VisibleForTesting
   Button button;
 
@@ -60,16 +62,56 @@ public class AppointmentBookGwt implements EntryPoint {
               sb.append("\n");
             }
             alerter.alert(sb.toString());
+
           }
         });
       }
     });
+
+    TextBox ownername = new TextBox();
+    ownername.setName("Owner");
+    ownername.setFocus(true);
+    Label ownerlabel = new Label("Owner:");
+    ownerlabel.setWidth("50%");
+    HorizontalPanel ownerpanel = new HorizontalPanel();
+//    ownerpanel.setWidth("100%");
+    ownerpanel.add(ownerlabel);
+    ownerpanel.add(ownername);
+
+
+    TextBox description = new TextBox();
+    description.setName("Description");
+    Label descriptionlabel = new Label("Description:");
+    descriptionlabel.setWidth("50%");
+    HorizontalPanel descriptionpanel = new HorizontalPanel();
+//    descriptionpanel.setWidth("100%");
+    descriptionpanel.add(descriptionlabel);
+    descriptionpanel.add(description);
+
+    tabpanel.setWidth("100%");
+    vpaneltab1.add(ownerpanel);
+    vpaneltab1.add(descriptionpanel);
+    vpaneltab1.setWidth("100%");
+
+    vpaneltab1.setCellHeight(ownerpanel,"10%");
+    vpaneltab1.setCellHeight(descriptionpanel,"10%");
+    tabpanel.add(vpaneltab1,"Add Appointment");
+    button.setWidth("50%");
+    button.setHeight("20%");
+    tabpanel.add(button,"Ping Button");
+    tabpanel.setHeight("90%");
+//    vpaneltab1.add(description);
+//    hpanelmain.add(vpaneltab1);
+//    hpanelmain.add(button);
+//    hpanelmain.setWidth("");
+
   }
 
   @Override
   public void onModuleLoad() {
-    RootPanel rootPanel = RootPanel.get();
-    rootPanel.add(button);
+    RootLayoutPanel rootPanel = RootLayoutPanel.get();
+//    rootPanel.add(button);
+    rootPanel.add(tabpanel);
   }
 
   @VisibleForTesting
