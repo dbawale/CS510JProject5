a728f52fe93bc86a64a03d79baa532160ad27edf
Deven Bawale <dbawale@pdx.edu>
2016-08-07 16:53:55 -0700

    Added JavaDoc comments to PingServiceAsync.java

--- apptbook-gwt/src/main/java/edu/pdx/cs410J/dbawale/client/PingServiceAsync.java
+++ apptbook-gwt/src/main/java/edu/pdx/cs410J/dbawale/client/PingServiceAsync.java
@@ -15,11 +15,31 @@ public interface PingServiceAsync {
    */
   void ping(AsyncCallback<AppointmentBook> async);
 
+  /**
+   * Return the AppointmentBook on the server
+   * @param async The asynchronous callback
+     */
   void getAppts(AsyncCallback<AppointmentBook> async);
 
+  /**
+   * Add a new appointment to the AppointmentBook on server
+   * @param appt The appointment book to be added
+   * @param owner The owner of the appointment book
+   * @param async The asynchronous callback
+     */
   void addAppt(Appointment appt, String owner, AsyncCallback<Void> async);
 
+  /**
+   * Return the owner name of the AppointmentBook on server
+   * @param async The asynchronous callback
+     */
   void getOwner(AsyncCallback<String> async);
 
+  /**
+   * Search for appointments on the server
+   * @param start Starting date of search
+   * @param end Ending date of search
+   * @param async The asynchronous callback
+     */
   void search(Date start, Date end, AsyncCallback<ArrayList<Appointment>>async);
 }
